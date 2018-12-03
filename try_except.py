a29d03c89fdf6ee962d8478260869637c6402c89
SongXYZ <2476251460@qq.com>
2018-12-03 11:13:04 +0800

    import logging

--- try_except.py
+++ try_except.py
@@ -1,15 +1,20 @@
+import logging
+
+
 try:
     num = int(input("Input integer:"))
     result = 8/num
-except ZeroDivisionError:
+except ZeroDivisionError as e:
     # 错误类型1
-    print("Please > 0")
+    print("Please > 0: %s" % e)
+    print("Please > 0: {}".format(e))
+    logging.exception(e)
 except ValueError:
     # 错误类型2
     print("Please a Input integer")
-except Exception as result:
+except Exception as res:
     # 打印未知错误
-    print("Unknown error: %s" % result)
+    print("Unknown error: %s" % res)
 else:
     # 没有异常时执行的代码
     print(result)
